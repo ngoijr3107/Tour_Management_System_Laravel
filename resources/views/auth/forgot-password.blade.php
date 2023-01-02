0e014d0fbd3429581dcb4ab561e9683997cb2950
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-03 00:33:40 +0600

    logo added

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img width="180px" src="{{ asset('assets/img/logo.png')}}">
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
