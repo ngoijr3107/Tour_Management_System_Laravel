4589c1b73dade970a5e68b9a62ee820335bb7640
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-04 20:01:58 +0600

    premium package module

--- app/Http/Controllers/PremiumPackageController.php
+++ app/Http/Controllers/PremiumPackageController.php
@@ -4,7 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//Model added
+use App\Models\Local_host_services;
+use App\Models\User;
+
+
 class PremiumPackageController extends Controller
 {
-    //
+    
+    public function afterSelectedGuide($placeId,$packageId,$id)
+    {
+
+        $hostService=Local_host_services::where('id',$id)->first();
+
+        $hostProfile=User::where('id',$hostService->user_id)->first();
+
+        return view('tourist.premiumpackage.detail_info_before_payment');
+
+
+    }
+
+
 }
