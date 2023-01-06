ca1ce151c7b80c3f61a7691862b245fdc301be6d
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-06 12:19:53 +0600

    module design

--- app/Http/Controllers/SslCommerzPaymentController.php
+++ app/Http/Controllers/SslCommerzPaymentController.php
@@ -201,7 +201,7 @@ class SslCommerzPaymentController extends Controller
 
     public function success(Request $request)
     {
-        //echo "Transaction is Successful";
+        echo "Transaction is Successful";
 
         $tran_id = $request->input('tran_id');
         $amount = $request->input('amount');
--- resources/views/tourist/mail/ServiceNotificationEmail.blade.php
+++ resources/views/tourist/mail/ServiceNotificationEmail.blade.php
@@ -146,7 +146,7 @@
           <!-- start permission -->
           <tr>
             <td align="center" bgcolor="#e9ecef" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-              <p style="margin: 0;">You received this email because we received a service from you.</p>
+              <p style="margin: 0;">You received this email because we received a service for you.</p>
             </td>
           </tr>
           <!-- end permission -->
