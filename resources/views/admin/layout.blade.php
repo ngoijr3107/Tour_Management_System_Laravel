db0dac687b297b35ee528d8acc774d87aa1946b9
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-10 02:43:18 +0600

    admin layout

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -19,7 +19,7 @@
   <!-- inject:css -->
   <link rel="stylesheet" href="{{ asset('assets/admin/css/vertical-layout-light/style.css') }}">
   <!-- endinject -->
-  
+
 </head>
 <body>
   <div class="container-scroller">
@@ -357,7 +357,7 @@
         <!-- partial:partials/_footer.html -->
         <footer class="footer">
           <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2023.  Pr <a href="{{ url('/') }}" target="_blank"> Gurta Jabo</a> . All rights reserved.</span>
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2023. <a href="{{ url('/') }}" target="_blank"> Gurta Jabo</a> . All rights reserved.</span>
             <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Gurta Jabo <i class="ti-heart text-danger ml-1"></i></span>
           </div>
         </footer>
