58395d81e47b2b3eb15f35de48e98cc29577924e
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-09 15:00:27 +0600

    virtual assistant added

--- resources/views/tourist/history.blade.php
+++ resources/views/tourist/history.blade.php
@@ -17,7 +17,9 @@
       <th scope="col">Period</th>
       <th scope="col">Place</th>
       <th scope="col">Tran No</th>
+      <th scope="col">Tour Status</th>
       <th scope="col">Payment Copy</th>
+      <th scope="col">Virtual Assistant Service</th>
     </tr>
   </thead>
   <tbody>
@@ -26,9 +28,18 @@
 
             <td scope="row" data-label="Period">{{ $history->from_date }} - {{ $history->to_date }}</td>
             <td data-label="Place">{{ $history->place->name }}</td>
-            <td data-label="Transaction No">{{ $history->transaction_id }}</td>
+            <td data-label="Tour Status">{{ $history->transaction_id }}</td>
+            <td data-label="Transaction No">{{ $history->tour_status }}</td>
             <td data-label="Payment Copy"><a href="{{ url('/download/payment-copy/'.$history->id) }}" class="btn btn-success">Download</a></td>
+            @if($history->package_id=='3' || $history->package_id=='4')
             
+              <td data-label="Virtual Assistant Service"><a href="{{ url('http://localhost:3000/?transactionId='.$history->transaction_id) }}"  target="_blank" class="btn btn-danger">Get App</a></td>
+            
+            @else
+
+              <td data-label="Virtual Assistant Service">Not Available</td>  
+            
+            @endif
         </tr>
     @endforeach
  
