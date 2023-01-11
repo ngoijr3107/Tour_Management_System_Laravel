30e792d00c207f437c4c5adac7be1d758c9c0100
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-11 22:49:19 +0600

    admin layout

--- resources/views/errorPage/404.blade.php
+++ resources/views/errorPage/404.blade.php
@@ -58,11 +58,12 @@
   <!-- container-scroller -->
   <!-- plugins:js -->
   <script src="{{ asset('assets/admin/vendors/js/vendor.bundle.base.js') }}"></script>
-  
+
   <!-- endinject -->
   <!-- Plugin js for this page -->
   <!-- End plugin js for this page -->
   <!-- inject:js -->
+  
   <script src="{{ asset('assets/admin/js/off-canvas.js') }}"></script>
   <script src="{{ asset('assets/admin/js/hoverable-collapse.js') }}"></script>
   <script src="{{ asset('assets/admin/js/template.js') }}"></script>
