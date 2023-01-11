87347d2b28d2122fb82d504bc49cb3c793d9dd3e
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-11 22:48:04 +0600

    admin layout

--- resources/views/errorPage/404.blade.php
+++ resources/views/errorPage/404.blade.php
@@ -6,11 +6,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Gurta Jabo</title>
+  
   <!-- plugins:css -->
   <link rel="stylesheet" href="{{ asset('assets/admin/vendors/feather/feather.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/admin/vendors/ti-icons/css/themify-icons.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/admin/vendors/css/vendor.bundle.base.css') }}">
-  
+
   <!-- endinject -->
   <!-- Plugin css for this page -->
   <!-- End plugin css for this page -->
