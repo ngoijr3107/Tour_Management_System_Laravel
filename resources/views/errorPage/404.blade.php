8c08acbb315fa603528f48162f69f0464c820ce5
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-11 22:47:00 +0600

    admin layout

--- resources/views/errorPage/404.blade.php
+++ resources/views/errorPage/404.blade.php
@@ -10,11 +10,12 @@
   <link rel="stylesheet" href="{{ asset('assets/admin/vendors/feather/feather.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/admin/vendors/ti-icons/css/themify-icons.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/admin/vendors/css/vendor.bundle.base.css') }}">
+  
   <!-- endinject -->
   <!-- Plugin css for this page -->
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  
+
   <link rel="stylesheet" href="{{ asset('assets/admin/css/vertical-layout-light/style.css') }}">
   <!-- endinject -->
 </head>
