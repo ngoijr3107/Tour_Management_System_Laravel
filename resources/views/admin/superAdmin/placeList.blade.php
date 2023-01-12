f77316a89a669dbea72007010fbd4ad62d0a2a3f
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-13 01:56:11 +0600

    super admin section

--- resources/views/admin/superAdmin/placeList.blade.php
+++ resources/views/admin/superAdmin/placeList.blade.php
@@ -3,7 +3,7 @@
 @section('container')
 
     
-    <h3 class="card-title">Service</h3>
+    <h3 class="card-title">Place</h3>
 
     <br>
 
@@ -11,30 +11,18 @@
 
    
 
-    @foreach($services as $service)
+    @foreach($places as $place)
 
         <div class="col-md-4">
 
             <div class="card" style="width: 18rem;">
-            @can('isLocalGuide')
-            <img class="card-img-top" src="{{ asset('assets/lgHotelRoomImage/'.$service->room_picture) }}" alt="Card image cap">
-            @endcan
-            @can('isLocalHost')
-            <img class="card-img-top" src="{{ asset('assets/lhRoomImage/'.$service->room_picture) }}" alt="Card image cap">
-            @endcan
+
+            <img class="card-img-top" src="{{ asset('assets/placeImage/'.$place->photo) }}" alt="Card image cap">
+
             <div class="card-body">
-                <h5 class="card-title">{{ $service->service_name }}</h5>
-                <p class="card-text">Total Price : {{ $service->total_price }}</p>
-                <p class="card-text"> <i>Rating :</i> {{ $service->rating }}</p>
-                @if($service->available == "Yes")
-
-                    <p class="text-success card-text">Available</p>
-
-                @else
-
-                <p class="text-danger card-text">Not Available</p>
-
-                @endif
+                <h5 class="card-title">{{ $place->name }}</h5>
+                <p class="card-text">Address : {{ $place->address }}</p>
+               
                 <br>
                 <a href="#" class="btn btn-primary">View</a>
                 <a href="#" class="btn btn-success">Edit</a>
