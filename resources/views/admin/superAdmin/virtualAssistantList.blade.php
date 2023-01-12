7a296b286de27a4a8f03cba91d2d2ae20c926bde
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-12 22:34:20 +0600

    super admin section

---
+++ resources/views/admin/superAdmin/virtualAssistantList.blade.php
@@ -0,0 +1,80 @@
+@extends('admin/layout')
+
+@section('container')
+
+
+
+    <div class="row">
+
+   
+
+   
+        <div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                    <h4 class="card-title">Virtual Assistant</h4>
+                    <p class="card-description">
+
+
+                    </p>
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                        <thead>
+                            <tr>
+                            <th>
+                                User
+                            </th>
+                            <th>
+                                Name
+                            </th>
+                            <th>
+                                Email
+                            </th>
+                            <th>
+                                Phone
+                            </th>
+                            <th>
+                                Address
+                            </th>
+                          
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            @foreach($virtualAssistant as $virtualAssistant)
+
+                                <tr>
+                                <td class="py-1">
+                                    <img src="{{ $virtualAssistant->profile_photo_url }}" alt="image"/>
+                                </td>
+                                <td>
+                                    {{ $virtualAssistant->name }}
+                                </td>
+                                <td>
+
+                                    {{ $virtualAssistant->email }}
+
+                                </td>
+                                <td>
+                                    {{ $virtualAssistant->phone }}
+                                </td>
+                                <td>
+                                    <a class="btn btn-rounded btn-success" href="{{ url('local-virtualAssistant/list/'. $virtualAssistant->id) }}">Details</a>
+                                </td>
+                                </tr>
+                            
+                             
+                            @endforeach
+                        </tbody>
+                        </table>
+                    </div>
+                    </div>
+                </div>
+                </div>
+        
+    
+
+     
+    </div>
+
+@endsection()
