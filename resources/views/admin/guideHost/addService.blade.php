110a5cf292f7a86aaa5be020b5e9e0f6cc89d0e5
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-15 01:01:40 +0600

    super admin section

--- resources/views/admin/guideHost/addService.blade.php
+++ resources/views/admin/guideHost/addService.blade.php
@@ -11,7 +11,19 @@
                     <p class="card-description">
                         Add Service
                     </p>
-                    
+
+                    @if ($errors->any())
+
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+
+                    @endif
+
                     @if(Session::has('success'))
 
                         <div class="alert alert-success">
