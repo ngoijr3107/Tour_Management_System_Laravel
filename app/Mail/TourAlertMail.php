952db6c74e9e1739469c8fc88d0c18866d5c5207
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-16 00:27:54 +0600

    super admin section

--- app/Mail/TourAlertMail.php
+++ app/Mail/TourAlertMail.php
@@ -11,14 +11,16 @@ class TourAlertMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $details;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($details)
     {
-        //
+        $this->details = $details;
     }
 
     /**
@@ -28,6 +30,7 @@ class TourAlertMail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->subject('Tour Alert Email')
+        ->view('tourist.mail.TourAlertEmail');
     }
 }
