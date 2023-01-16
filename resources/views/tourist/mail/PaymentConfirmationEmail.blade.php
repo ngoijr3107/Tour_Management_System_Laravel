fc2a8d3793de18838ffa2a17c285e76f1f67fad1
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-17 00:21:48 +0600

    super admin section

--- resources/views/tourist/mail/PaymentConfirmationEmail.blade.php
+++ resources/views/tourist/mail/PaymentConfirmationEmail.blade.php
@@ -32,7 +32,7 @@
           <tr>
             <td align="center" valign="top" style="padding: 36px 24px;">
              
-                <h1>Gurta Jabo</h1>
+                <h1>Gurte Jabo</h1>
       
             </td>
           </tr>
@@ -128,7 +128,7 @@
           <!-- start copy -->
           <tr>
             <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
-              <p style="margin: 0;">Founder,<br> Gurta Jabo</p>
+              <p style="margin: 0;">Founder,<br> Gurte Jabo</p>
             </td>
           </tr>
           <!-- end copy -->
