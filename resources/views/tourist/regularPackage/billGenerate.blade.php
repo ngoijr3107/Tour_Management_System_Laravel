4c8e8290b4a74b61b9af6c57c6c4f0b3fec9f0ce
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-17 22:56:16 +0600

    super admin section

--- resources/views/tourist/regularPackage/billGenerate.blade.php
+++ resources/views/tourist/regularPackage/billGenerate.blade.php
@@ -139,7 +139,7 @@
     </div>
 
     <footer class="my-5 pt-5 text-muted text-center text-small">
-        <p class="mb-1">&copy; 2023 Gurte Jabo</p>
+        <p class="mb-1">&copy; 2023 Ghurte Jabo</p>
         <ul class="list-inline">
             <li class="list-inline-item"><a href="/">Home</a></li>
             <li class="list-inline-item"><a href="/#service">Service</a></li>
