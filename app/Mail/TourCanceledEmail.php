6852d07310907ec8a069ec9d85d4bccbd33e55b2
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-20 16:22:20 +0600

    status check

--- app/Mail/TourCanceledEmail.php
+++ app/Mail/TourCanceledEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class TourCancelEmail extends Mailable
+class TourCanceledEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
