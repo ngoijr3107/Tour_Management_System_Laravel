737a3e47d81ac43367c11caf313d1ffcf2f39700
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-21 23:00:01 +0600

    status check

--- routes/web.php
+++ routes/web.php
@@ -164,5 +164,6 @@ Route::middleware([
     Route::get('/return/booking/process/{id}', [SuperAdminController::class, 'returnBookingProcess'])->name('/return/booking/process');
     Route::post('/return/booking/confirm/{id}', [SuperAdminController::class, 'returnBookingConfirm'])->name('/return/booking/confirm');
     Route::get('/message/all', [SuperAdminController::class, 'messageList'])->name('/message/all');
+    Route::get('/all/banner', [SuperAdminController::class, 'bannerList'])->name('/all/banner');
 
 });
