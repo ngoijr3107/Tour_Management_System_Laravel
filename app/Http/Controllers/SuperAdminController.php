45399d6be446a4a9697820c0f3604b4ee3a7744c
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-21 23:09:34 +0600

    status check

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -10,6 +10,7 @@ use App\Models\Review;
 use App\Models\Local_guide_service;
 use App\Models\Local_host_service;
 use App\Models\Contact;
+use App\Models\Banner;
 
 use Illuminate\Http\Request;
 use Gate;
