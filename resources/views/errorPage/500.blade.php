a88133679adc948e27db4980b8d3cc7603d6b291
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-29 23:54:20 +0600

    super admin section

--- resources/views/errorPage/500.blade.php
+++ resources/views/errorPage/500.blade.php
@@ -25,6 +25,7 @@
     <div class="container-fluid page-body-wrapper full-page-wrapper">
       <div class="content-wrapper d-flex align-items-center text-center error-page bg-info">
         <div class="row flex-grow">
+          
           <div class="col-lg-7 mx-auto text-white">
             <div class="row align-items-center d-flex flex-row">
               <div class="col-lg-6 text-lg-right pr-lg-4">
@@ -50,7 +51,7 @@
                 <a class="text-white font-weight-medium" href="../../index.html">Back to home</a>
 
               </div>
-              
+
             </div>
 
             <div class="row mt-5">
