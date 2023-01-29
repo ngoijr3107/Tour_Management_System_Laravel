fe174e7a8ab493e7bb24b34569931f30b8ea92b7
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-29 23:56:08 +0600

    super admin section

--- resources/views/errorPage/500.blade.php
+++ resources/views/errorPage/500.blade.php
@@ -6,8 +6,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Skydash Admin</title>
-  <!-- plugins:css -->
   
+  <!-- plugins:css -->
+
   <link rel="stylesheet" href="../../vendors/feather/feather.css">
   <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
   <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css">
