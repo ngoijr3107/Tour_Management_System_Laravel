99fd4518a12931877d73a6683a3ebe21957dba33
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-01-30 00:02:44 +0600

    super admin section

--- resources/views/errorPage/404.blade.php
+++ resources/views/errorPage/404.blade.php
@@ -59,8 +59,9 @@
     </div>
 
     <!-- page-body-wrapper ends -->
-    
+
   </div>
+  
   <!-- container-scroller -->
   <!-- plugins:js -->
   <script src="{{ asset('assets/admin/vendors/js/vendor.bundle.base.js') }}"></script>
